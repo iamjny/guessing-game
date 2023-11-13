ada5d013907d8666da7aec34cce0ac3199eb8d12
iamjny <jjng435@gmail.com>
2023-11-13 14:10:31 -0500

    Main menu option for play mode

---
+++ words.py
@@ -0,0 +1,8 @@
+
+print("++")
+print("++ The great guessing game")
+print("++\n")
+print("Current guess: ")
+print("Letters guessed: ")
+print("g = guess, t = tell me, l for a letter, and q to quit")
+print("Enter Option: ")
