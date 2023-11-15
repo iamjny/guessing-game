273cfc0c0d048b80b8a14d65123d0be4e3c4b580
iamjny <jjng435@gmail.com>
2023-11-15 02:05:42 -0500

    Rewrote guess.py in OOP form

--- guess.py
+++ guess.py
@@ -1,67 +1,111 @@
-import StringDatabase
+import os
 
 
 class Guess:
-    pass
+    def __init__(self, string_database):
+        self.string_database = string_database
+        self.random_word = None
+        self.current_guess = ['-'] * 4
+        self.correct_guesses = set()
+        self.guessed_letters = []
 
+    # Generate new word for when the game is completed and moves on to the next
+    def generate_word(self):
+        self.random_word = self.string_database.random_word()
+        self.current_guess = ['-'] * len(self.random_word)
+        self.correct_guesses.clear()
+        self.guessed_letters.clear()
 
-def play_mode():
-    print("Play mode...\n")
-    print("++")
-    print("++ The great guessing game")
-    print("++\n")
+    # Displays the correct play/test mode menu
+    def menu(self, mode):
+        print("++")
+        print("++ The great guessing game")
+        print("++\n")
 
-    random_word = StringDatabase.random_word(StringDatabase.words)
+        if mode == "play":
+            print("Current Guess: " + ''.join(self.current_guess))
+        elif mode == "test":
+            print("Current word: " + self.random_word)
+            print("Current Guess: " + ''.join(self.current_guess))
 
-    print("Current guess: ----")
-    print("Letters guessed: ")
-    while True:
-        print("g = guess, t = tell me, l for a letter, and q to quit")
-        user_input = input("Enter Option: ").lower()
+    # Menu options
+    def play(self, mode):
+        self.generate_word()
 
-        if user_input == 'g':
-            guess = input("Enter your guess: ")
-            # Process the guess
-            print(f"You guessed: " + guess)
-        elif user_input == 't':
-            print(f"The word is: " + random_word)
-        elif user_input == 'l':
-            letter = input("Enter a letter: ")
-            # Process the letter input
-            print("You entered the letter: " + letter)
-        elif user_input == 'q':
-            print("Quitting game...")
-            break
+        while True:
+            self.menu(mode)
+            print("Letters guessed: " + ' '.join(self.guessed_letters))
+            print("g = guess, t = tell me, l for a letter, and q to quit")
+            user_input = input("\nEnter Option: ").lower()
+
+            if user_input == 'g':
+                guess = input("\nEnter your guess: ")
+                self.input_guess(guess)
+            elif user_input == 't':
+                self.tell_word()
+                input("Press Enter to continue...")
+                self.generate_word()
+                os.system('clear')
+            elif user_input == 'l':
+                letter = input("\nEnter a letter: ")
+                self.input_letter(letter)
+            elif user_input == 'q':
+                print("Quitting game...")
+                break
+            else:
+                print("\nInvalid option. Please choose again.\n\n")
+                input("Press Enter to continue...")
+                os.system('clear')
+
+    # When user chooses option 'g', user will be able to guess the word
+    def input_guess(self, guess):
+        if guess == self.random_word:
+            print("\n@@")
+            print("@@ FEEDBACK: You're right Einstein!")
+            print("@@\n\n")
+            self.generate_word()
+            input("Press Enter to continue...")
+            os.system('clear')
         else:
-            print("Invalid option. Please choose again.")
+            print("\n@@")
+            print("@@ FEEDBACK: Try again, loser!")
+            print("@@\n\n")
+            input("Press Enter to continue...")
+            os.system('clear')
 
+    # When user chooses option 't', the game will print the correct word
+    def tell_word(self):
+        print("\n@@")
+        print(f"@@ FEEDBACK: You really should have guessed this... '" + self.random_word + "'")
+        print("@@\n\n")
 
-def test_mode():
-    print("Test mode...\n")
-    print("++")
-    print("++ The great guessing game")
-    print("++\n")
+    # When user chooses option 'l', user will be able to guess a letter that might be in the word
+    def input_letter(self, letter):
+        if letter not in self.guessed_letters:
+            self.guessed_letters.append(letter)
 
-    random_word = StringDatabase.random_word(StringDatabase.words)
-    print("Current Word: " + random_word)
-    print("Current Guess: ----")
-    print("Letters guessed: ")
-    while True:
-        print("g = guess, t = tell me, l for a letter, and q to quit")
-        user_input = input("Enter Option: ").lower()
+        if letter in self.random_word and letter not in self.correct_guesses:
+            print("\n@@")
+            print("@@ FEEDBACK: Woo hoo, you found 1 letter!")
+            print("@@\n\n")
 
-        if user_input == 'g':
-            guess = input("Enter your guess: ")
-            # Process the guess
-            print(f"You guessed: " + guess)
-        elif user_input == 't':
-            print(f"The word is: " + random_word)
-        elif user_input == 'l':
-            letter = input("Enter a letter: ")
-            # Process the letter input
-            print("You entered the letter: " + letter)
-        elif user_input == 'q':
-            print("Quitting game...")
-            break
+            self.correct_guesses.add(letter)
+
+            for i in range(len(self.random_word)):
+                if self.random_word[i] == letter:
+                    self.current_guess[i] = letter
+
+            input("Press Enter to continue...")
+            os.system('clear')
+        elif letter in self.random_word and letter in self.correct_guesses and letter in self.guessed_letters:
+            print("\n@@")
+            print("@@ FEEDBACK: You've already used this letter, silly! Try again!")
+            print("@@\n\n")
+            input("Press Enter to continue...")
+            os.system('clear')
         else:
-            print("Invalid option. Please choose again.")
+            print("\n@@")
+            print("@@ FEEDBACK: Not a single match, loser!")
+            print("@@\n\n")
+            input("Press Enter to continue...")
+            os.system('clear')
