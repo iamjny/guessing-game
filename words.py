40132b42acac6862b87b8eb292288800c7269ecd
iamjny <jjng435@gmail.com>
2023-11-15 01:58:27 -0500

    Updated main script with OO calls

--- words.py
+++ words.py
@@ -1,17 +1,15 @@
 import sys
-import guess
-
-
-def menu():
-    mode = sys.argv[1]
-
-    if mode == "play":
-        guess.play_mode()
-    elif mode == "test":
-        guess.test_mode()
-    else:
-        print("Invalid game mode...quitting program...")
-
+from StringDatabase import StringDatabase
+from guess import Guess
 
 if __name__ == "__main__":
-    menu()
+    if len(sys.argv) < 2:
+        print("Please provide a game mode ('play' or 'test'). Exiting game...")
+    else:
+        mode = sys.argv[1]
+        if mode not in ["play", "test"]:
+            print("Invalid game mode. Exiting game...")
+        else:
+            string_database = StringDatabase('four_letters.txt')
+            game = Guess(string_database)
+            game.play(mode)
