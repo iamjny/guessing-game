5f267756d2736b3c79232412370319bb379e1e55
iamjny <jjng435@gmail.com>
2023-11-15 19:59:10 -0500

    Drafted the Game class with relevant functions

--- game.py
+++ game.py
@@ -1,2 +1,16 @@
+
 class Game:
-    pass
+    def __init__(self):
+        self.games = []
+
+    def add_game(self, word, status, bad_guesses, missed_letters, score):
+        game_record = {"word": word, "status": status, "bad_guesses": bad_guesses,
+                       "missed_letters": missed_letters, "score": score}
+        self.games.append(game_record)
+
+    def print_scores(self):
+        print("++")
+        print("++ Game Report")
+        print("++\n")
+        print("Game         Word        Status          Bad Guesses         Missed Letters          Score")
+        print("----         ----        ------          -----------         --------------          -----")
