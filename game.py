60ea718e345a304a43ccb7451bac9d12ac6b1113
iamjny <jjng435@gmail.com>
2023-11-17 20:05:07 -0500

    More progress on game.py functions

--- game.py
+++ game.py
@@ -1,11 +1,13 @@
-
 class Game:
     def __init__(self):
         self.games = []
 
-    def add_game(self, word, status, bad_guesses, missed_letters, score):
+    # def add_game(self, word, status, bad_guesses, missed_letters, score):
+    def add_game(self, word, status, bad_guesses, missed_letters):
+        # game_record = {"word": word, "status": status, "bad_guesses": bad_guesses,
+        #                "missed_letters": missed_letters, "score": score}
         game_record = {"word": word, "status": status, "bad_guesses": bad_guesses,
-                       "missed_letters": missed_letters, "score": score}
+                       "missed_letters": missed_letters}
         self.games.append(game_record)
 
     def print_scores(self):
@@ -14,3 +16,12 @@ class Game:
         print("++\n")
         print("Game         Word        Status          Bad Guesses         Missed Letters          Score")
         print("----         ----        ------          -----------         --------------          -----")
+        for i, game in enumerate(self.games, start=1):
+            print(
+                str(i) + "            " + str(game["word"]) + "        " + str(game["status"]) + "         " + str(game[
+                                                                                                                       "bad_guesses"]) + "                   " +
+                str(game["missed_letters"]))
+        print("\n")
+
+    def calculate_scores(self):
+        pass
