4ae92fe894fa02106d780a9a7dbd321d8c6eb1d0
iamjny <jjng435@gmail.com>
2023-11-18 20:50:58 -0500

    Implemented new score calculation function

--- game.py
+++ game.py
@@ -23,13 +23,54 @@ class Game:
         print("++\n")
         print("Game         Word        Status          Bad Guesses         Missed Letters          Score")
         print("----         ----        ------          -----------         --------------          -----")
-        for i, game in enumerate(self.games, start=1):
-            print(
-                str(i) + "            " + str(game["word"]) + "        " + str(game["status"]) + "         " + str(
-                    game["bad_guesses"]) + "                   " +
-                str(game["missed_letters"]) + "                       " + str(game["score"]))
-        print("\n")
 
-    def calculate_score_t(self, word):
-        score = -(sum(self.letter_frequencies[letter] for letter in word))
-        return score
+        # If user quits without completing game, it will still print the scoreboard but with no rows and a final
+        # score of 0.
+        # Else, print the normal scoreboard
+        if not self.games:
+            print("             " + "            " + "                " + "                              " +
+                  "              0")
+        else:
+            for i, game in enumerate(self.games, start=1):
+                print(
+                    str(i) + "            " + str(game["word"]) + "        " + str(game["status"]) + "         " + str(
+                        game["bad_guesses"]) + "                   " +
+                    str(game["missed_letters"]) + "                       " + str(game["score"]))
+            print("\n")
+
+    def calculate_score(self, guess_word, real_word, bad_letter_guesses, bad_word_guesses, give_up=False):
+        if give_up:
+            # Calculate the sum of letter frequencies for covered letters and turn it into negative
+            # when user gives up (selection option 't')
+            covered_letters = set(real_word) - set(''.join(guess_word).replace("-", ""))
+
+            return -sum(self.letter_frequencies[letter] for letter in covered_letters)
+
+        if bad_letter_guesses == 0 and bad_word_guesses == 0:
+            # If no bad letter guesses and no bad word guesses, return the sum of all letter frequencies
+            return sum(self.letter_frequencies[letter] for letter in real_word)
+
+        missing_letters = [letter for letter in real_word if letter not in guess_word]
+
+        # Calculate the score for missing letters
+        score = sum(self.letter_frequencies[letter] for letter in missing_letters)
+
+        if bad_letter_guesses != 0:
+            # First calculation with sum of missing letters divide by missed letters
+            calculation_1 = score / bad_letter_guesses
+        else:
+            # Edge case not mentioned in instructions or by prof:
+            # In the scenario where user makes more than 1 bad word guesses but 0 letter guesses,
+            # I have decided that the program will just calculate the sum of letter frequencies
+            # of all letters (since no letter guesses have been made / they're all covered)
+            # without dividing by # missed letter guesses (since you cannot divide by 0).
+            score_no_letter = sum(self.letter_frequencies[letter] for letter in real_word)
+            return score_no_letter
+
+        # Second calculation where you take off 10% for every bad word guesses from
+        # the first calculation right above
+        discount_percentage = 0.1
+        discount = discount_percentage * bad_word_guesses
+        calculation_2 = calculation_1 - (calculation_1 * discount)
+
+        return calculation_2
